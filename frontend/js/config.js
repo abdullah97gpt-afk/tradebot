ffcd4ba39567145dedee6e391b62e8bb323d47d1
Abdullah <abdullah97gpt@gmail.com>
2026-05-14 01:23:04 +0300

    Fix Supabase project URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
-﻿export const SUPABASE_CONFIG = {
-  url: "https://gdwyzdmtipmomjzappam.supabase.co/rest/v1/",
+export const SUPABASE_CONFIG = {
+  url: "https://gdwyzdmtipmomjzappam.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkd3l6ZG10aXBtb21qemFwcGFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2OTQyODUsImV4cCI6MjA5NDI3MDI4NX0.JQHQhzj43dhXrKSv8y6TBoFbt7XXSHIQ6jUsVh6D6DA"
 };
 
